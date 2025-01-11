ed3cfc3af516f4b5ce02916c02399f795cd585c2
matheodepuydt <150126156+matheodepuydt@users.noreply.github.com>
2025-01-11 22:47:59 +0100

    css page joueur

--- Fichiers_PHP/Vues/page_joueursVue.php
+++ Fichiers_PHP/Vues/page_joueursVue.php
@@ -17,11 +17,11 @@
     include('../static/header.html');
     ?>
 
-    <div>
+    <div class="players-page-container">
         <h1>Joueurs</h1>
         <label for="joueurs">Liste des joueurs :</label>
 
-        <table>
+        <table class="players-table">
             <thead>
                 <tr>
                     <th>Nom</th>
@@ -40,23 +40,21 @@
 
                 foreach ($joueurs as $joueur) {
                     echo "<tr>
-                            <form method='POST' action=''>
-                                <td><input type='hidden' name='nom' value='{$joueur['nom']}'>{$joueur['nom']}</td>
-                                <td><input type='hidden' name='prenom' value='{$joueur['prenom']}'>{$joueur['prenom']}</td>
-                                <td><input type='hidden' name='date_de_naissance' value='{$joueur['date_de_naissance']}'>{$joueur['date_de_naissance']}</td>
-                                <td><input type='hidden' name='taille' value='{$joueur['taille']}'>{$joueur['taille']}</td>
-                                <td><input type='hidden' name='poids' value='{$joueur['poids']}'>{$joueur['poids']}</td>
-                                <td><input type='hidden' name='statut' value='{$joueur['statut']}'>{$joueur['statut']}</td>
-                                <td><input type='hidden' name='licence' value='{$joueur['num_licence']}'>{$joueur['num_licence']}</td>
-                                <td><button type='submit' name='commentaire'>Ajouter un commentaire</button></td>
-                                <td><button type='submit' name='supprimer'>Supprimer</button></td>
-                            </form>
+                            <td><input type='hidden' name='nom' value='{$joueur['nom']}'>{$joueur['nom']}</td>
+                            <td><input type='hidden' name='prenom' value='{$joueur['prenom']}'>{$joueur['prenom']}</td>
+                            <td><input type='hidden' name='date_de_naissance' value='{$joueur['date_de_naissance']}'>{$joueur['date_de_naissance']}</td>
+                            <td><input type='hidden' name='taille' value='{$joueur['taille']}'>{$joueur['taille']}</td>
+                            <td><input type='hidden' name='poids' value='{$joueur['poids']}'>{$joueur['poids']}</td>
+                            <td><input type='hidden' name='statut' value='{$joueur['statut']}'>{$joueur['statut']}</td>
+                            <td><input type='hidden' name='licence' value='{$joueur['num_licence']}'>{$joueur['num_licence']}</td>
+                            <td><button type='submit' name='commentaire'>Ajouter un commentaire</button></td>                                
+                            <td><button type='submit' name='supprimer'>Supprimer</button></td>
                           </tr>";
                 }
                 ?>
             </tbody>
         </table>
-            <div>
+            <div class="add-player-section">
                 <a href="ajouterJoueurVue.php">
                     <input type="button" value="Ajouter"/>
                 </a>
