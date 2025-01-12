36b39a8d11e48cfaf13e3fa7a743bbc2eb45a91b
Spleedz <maxence.beth@etu.iut-tlse3.fr>
2025-01-12 14:27:20 +0100

    Update des controleurs déjà fait pour utiliser le singleton

--- Fichiers_PHP/Controleurs/joueursControleur.php
+++ Fichiers_PHP/Controleurs/joueursControleur.php
@@ -2,35 +2,9 @@
 
 class controleurJoueurs{
     
-    private $server = 'localhost';
-    private $bd = 'phphandball';
-    private $db_login = 'admin';
-    private $db_password = '$iutinfo';
-    private $linkpdo;
-    
-    /**
-    * Méthode pour établir la connexion à la base de données
-    */
-    public function connectionBD() {
-        if ($this->linkpdo === null) {
-            try {
-                $this->linkpdo = new PDO(
-                    "mysql:host={$this->server};dbname={$this->bd}", 
-                    $this->db_login, 
-                    $this->db_password
-                );
-                $this->linkpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                die('Erreur de connexion : ' . $e->getMessage());
-            }
-        }
-    }
-    
     public function getAllJoueurs(){
 
-        if (!$this->linkpdo) {
-            $this->connectionBD();
-        }
+        $db = Database::getInstance()->getConnection();
 
         // Préparation de la requête
         $stmt = $this->linkpdo->prepare('SELECT * FROM joueur');
@@ -42,9 +16,8 @@ class controleurJoueurs{
     }
 
     public function getJoueur($num_licence){
-        if (!$this->linkpdo) {
-            $this->connectionBD();
-        }
+
+        $db = Database::getInstance()->getConnection();
 
         // Préparation de la requête
         $stmt = $this->linkpdo->prepare('SELECT * FROM joueur where num_licence = :num_licence');
@@ -56,9 +29,8 @@ class controleurJoueurs{
     }
 
     public function modifierJoueur(Joueur $joueur){
-        if (!$this->linkpdo) {
-            $this->connectionBD();
-        }
+
+        $db = Database::getInstance()->getConnection();
 
         // Préparation de la requête
         $stmt = $this->linkpdo->prepare('
@@ -81,9 +53,8 @@ class controleurJoueurs{
     }
 
     public function addJoueur(Joueur $joueur){
-        if (!$this->linkpdo) {
-            $this->connectionBD();
-        }
+        
+        $db = Database::getInstance()->getConnection();
 
         // Préparation de la requête
         $stmt = $this->linkpdo->prepare('
